189c3916a73489df6f9b86b0569dd77fc52ecaba
metal <metalheadkrn@gmail.com>
2025-12-10 19:16:43 +0530

    Printing Values of variables in list and maps

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,16 @@
 output "v1" {
   value = var.v1
 }
+
+output "First value in List" {
+  value = var.v2[0]
+}
+
+output "Second value in List" {
+  value = var.v2[1]
+}
+
+output "Value in Map" {
+  value = var.v3["abc"]
+}
+
