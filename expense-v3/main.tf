4666d658fd70f0053d51bf2773082828344262c3
metal <metalheadkrn@gmail.com>
2025-12-11 19:37:48 +0530

    Creating all instances and records in expense project through loops

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -17,13 +17,12 @@ resource "aws_instance" "instances" {
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "records" {
   count                  = length(var.components)
   zone_id = var.zone_id
   name    = "${element(var.components,count.index)}-dev"
   type    = "A"
   ttl     = 30
-
   records = [element(aws_instance.instances.*.private_ip,count.index)]
 }
 
