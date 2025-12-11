22ca41b2360f755cfd030cf09b6ae0b45d9a8cd4
metal <metalheadkrn@gmail.com>
2025-12-11 13:49:53 +0530

    Creating instances frontend, backend and mysql through terraform

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -2,27 +2,29 @@ resource "aws_instance" "frontend" {
   ami                    =  "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
-}
+
   tags = {
     Name = "frontend-dev"
   }
+}
 
 resource "aws_instance" "backend" {
   ami                    =  "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
-}
+
 tags = {
   Name = "backend-dev"
+ }
 }
 
 resource "aws_instance" "mysql" {
-  ami                    =  "ami-0b4f379183e5706b9"
-  instance_type          = "t3.micro"
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
-}
-tags = {
-  Name = "mysql-dev"
-}
 
+  tags = {
+    Name = "mysql-dev"
+  }
+}
 
