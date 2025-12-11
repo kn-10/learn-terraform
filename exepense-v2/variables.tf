2b28ee5e25f4c683c0c593b8d0d98de2cb3c0ee3
metal <metalheadkrn@gmail.com>
2025-12-11 16:57:34 +0530

    Fetching ami id through data sources and use in main.tf

---
+++ exepense-v2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0a13f9496e8f730c7"]
+}
+
+variable "zone_id" {
+  default = "Z0266758558URTEO39RC"
+}
