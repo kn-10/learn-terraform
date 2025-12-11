ed47e9c3b711b7f995b79ca1ee9e83015b44bfa5
metal <metalheadkrn@gmail.com>
2025-12-11 14:43:34 +0530

    Creating instances frontend, backend and mysql and also DNS records through terraform

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
 resource "aws_instance" "frontend" {
   ami                    =  "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
@@ -7,6 +15,14 @@ resource "aws_instance" "frontend" {
     Name = "frontend-dev"
   }
 }
+
+resource "aws_route53_record" "frontend" {
+  name                  = "frontend-dev"
+  type                  = "A"
+  zone_id               = "Z0266758558URTEO39RC"
+  records               = [aws_instance.frontend.private_ip]
+}
+
 
 resource "aws_instance" "backend" {
   ami                    =  "ami-0b4f379183e5706b9"
@@ -18,13 +34,29 @@ tags = {
  }
 }
 
+resource "aws_route53_record" "backend" {
+  name                  = "backend-dev"
+  type                  = "A"
+  zone_id               = "Z0266758558URTEO39RC"
+  records               = [aws_instance.backend.private_ip]
+}
+
+
 resource "aws_instance" "mysql" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t3.micro"
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
 
   tags = {
     Name = "mysql-dev"
   }
+}
+
+
+resource "aws_route53_record" "mysql" {
+  name                  = "mysql-dev"
+  type                  = "A"
+  zone_id               = "Z0266758558URTEO39RC"
+  records               = [aws_instance.mysql.private_ip]
 }
 
