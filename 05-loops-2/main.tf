37190f15c07e5bf45aac911efef11cf322d47e6a
metal <metalheadkrn@gmail.com>
2025-12-12 12:15:54 +0530

    For each loop example demo

---
+++ 05-loops-2/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+variable "fruits1" {
+  default = {
+    apple = 10
+    banana = 100
+  }
+}
+
+variable "fruits2" {
+  default = {
+    apple = {
+      name = "apple"
+      quantity = 10
+    }
+  }
+}
+
+resource "null_resource" "test1" {
+  for_each = var.fruits1
+  provisioner "local-exec" {
+    command = "echo ${each.key} = ${each.value}"
+  }
+}
+
+resource "null_resource" "test2" {
+  for_each = var.fruits2
+  provisioner "local-exec" {
+    command = "echo ${lookup(each.value, "name", "null")}" = ${lookup(each.value, "quantity", "null")}"
+  }
+}
